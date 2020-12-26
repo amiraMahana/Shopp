bcb73b5b5cdd54562cd35cbcbd9fd59a6fefabb7
Amira <amiramahana9@gmail.com>
2020-12-26 16:07:04 +0300

    second

--- sever.js
+++ sever.js
@@ -3,6 +3,6 @@ const app=express()
 const path=require('path')
 app.use(express.static(__dirname+"/dist/shop"))
 app.listen(process.env.PORT || 3000)
-app.get('/',(req,res)=>{
+app.get('/*',(req,res)=>{
     res.sendFile(path.join(__dirname +"'dist/shop/index.html"))
 })
